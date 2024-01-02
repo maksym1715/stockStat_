ea993a0b370aa3300591d7938f3b10285424e01b
Lenovo <Lenovo@10.100.102.8>
2024-01-02 22:51:40 +0200

    commit 7

--- stockStat/src/main/java/team606/stockStat/communication/parser/CsvYahooParserImpl.java
+++ stockStat/src/main/java/team606/stockStat/communication/parser/CsvYahooParserImpl.java
@@ -33,24 +33,26 @@ public class CsvYahooParserImpl implements CsvParser{
     public List<CsvData> parseCsvFile(MultipartFile file, String fromDate, String toDate) throws IOException, ParseException, CsvException{
         Date fromDateObj = dateFormat.parse(fromDate);
         Date toDateObj = dateFormat.parse(toDate);
-        String indexName = file.getOriginalFilename().split(".")[0];
+        String indexName = file.getOriginalFilename().split("\\.")[0];
         List<CsvData> dataList = new ArrayList<>();
 
         try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
-            String[] line;
+            reader.readNext();
+            String[] line = reader.readNext();
             while ((line = reader.readNext()) != null) {
                 CsvData data = new CsvData();
                 UploadInfo uploadInfo = new UploadInfo();
                 uploadInfo.setDate(LocalDate.parse(line[0]));
-                uploadInfo.setSource(line[1]);
+                uploadInfo.setSource(indexName);
                 uploadInfo = uploadInfoRepository.save(uploadInfo); 
                 data.setUploadInfoId(uploadInfo);
                 try {
-                    data.setClose(Double.parseDouble(line[2]));
-                    data.setVolume(Double.parseDouble(line[3]));
-                    data.setOpen(Double.parseDouble(line[4]));
-                    data.setHigh(Double.parseDouble(line[5]));
-                    data.setLow(Double.parseDouble(line[6]));
+                    data.setClose(Double.parseDouble(line[4]));
+                    data.setVolume(Double.parseDouble(line[6]));
+                    data.setOpen(Double.parseDouble(line[1]));
+                    data.setHigh(Double.parseDouble(line[2]));
+                    data.setLow(Double.parseDouble(line[3]));
+                    data.setUploadInfoId(uploadInfo);
                 } catch (NumberFormatException e) {
                     e.printStackTrace(); 
                 }                
@@ -58,12 +60,12 @@ public class CsvYahooParserImpl implements CsvParser{
                 LocalDate dateString = uploadInfo.getDate();
                 Date dataDate = null;
 
-//                try {
-//                    dataDate = dateFormat.parse(dateString);
-//                } catch (ParseException e) {
-//                    e.printStackTrace();
-//                }
-//
+                try {
+                    dataDate = dateFormat.parse(String.valueOf(dateString));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
                 
                 if (dataDate != null && dataDate.compareTo(fromDateObj) >= 0 && dataDate.compareTo(toDateObj) <= 0) {
                     dataList.add(data);
--- stockStat/src/main/java/team606/stockStat/communication/service/PeriodServiceImpl.java
+++ stockStat/src/main/java/team606/stockStat/communication/service/PeriodServiceImpl.java
@@ -1,20 +1,18 @@
 package team606.stockStat.communication.service;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import team606.stockStat.communication.dto.PeriodData;
 import team606.stockStat.communication.dto.PeriodRequest;
-@Service
-public class PeriodServiceImpl implements  PeriodService{
 
-	public List<PeriodData> getPeriodBetween(PeriodRequest request) {
-        //Maksim have to check.
+@Service
+public class PeriodServiceImpl implements PeriodService {
+
+
+    public List<PeriodData> getPeriodBetween(PeriodRequest request) {
+        return null;
     }
 
-	
 
 }
